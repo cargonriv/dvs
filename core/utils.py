6839c03c471f57299911b7f976532598b03d76f5
Carlos Gonzalez Rivera <carlos.gonzalezrivera@pnnl.gov>
2022-01-27 22:00:06 -0800

    utils.py update

--- core/utils.py
+++ core/utils.py
@@ -6,3 +6,15 @@ class coordinate_point:
         self.polarity = polarity
     def __repr__(self):
         return repr((self.time, self.x_point, self.y_point, self.polarity))
+
+def poker_polarity_separator(data):
+    data_points = data
+    points_0_polarity = []
+    points_1_polarity = []
+    for coordinate_points in data_points:
+        polarity = coordinate_points[3]
+        if polarity == 1:
+            points_0_polarity.append(coordinate_points)
+        else:
+            points_1_polarity.append(coordinate_points)
+    return points_0_polarity, points_1_polarity
